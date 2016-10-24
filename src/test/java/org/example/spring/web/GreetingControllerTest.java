afbdfa3cb5635c5f1bc0cea7bc1448859ceca3bd
Zagrey <zagrebin@gmail.com>
2016-10-24 09:44:51 +0300

    tests

---
+++ src/test/java/org/example/spring/web/GreetingControllerTest.java
@@ -0,0 +1,26 @@
+package org.example.spring.web;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by zagrey on 23.10.2016.
+ */
+public class GreetingControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getGreetings() throws Exception {
+
+    }
+
+}
